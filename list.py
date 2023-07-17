5f1c3fe9c20cefdf7f7c85c1323f1bf5ed392750
Ashwini Mathpati <110845470+MathpatiAshwini@users.noreply.github.com>
2023-07-17 12:35:03 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,12 @@
+# a=int(input("enter the number"))
+# for i in range(1,(a+1)):
+#     print("\n")
+#     for j in range(1,11):
+#         print(i,"*",j,"=",i*j)
+
+# var a = require
+
+
+for (var i=1;i<=10;i++){
+    console.log(i)
+}
